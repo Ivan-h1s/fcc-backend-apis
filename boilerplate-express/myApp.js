d05d235ae6440a16ddc853ac8a1a6bd4b0b188cc
Ivan Altamirano <pedrotrill@gmail.com>
2023-02-08 07:25:04 -0300

    arreglo error

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,11 +1,6 @@
 let express = require('express');
 let app = express();
-let server = require('./server');
 
 console.log("Hello World");
-
-// app.get('/', (req, res) => {
-//     console.log("Hello World");
-// });
 
 module.exports = app;
