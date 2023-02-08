4b261016075e07a8077197cdce01197ae75b9c42
Ivan Altamirano <pedrotrill@gmail.com>
2023-02-08 09:34:12 -0300

    env

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,6 +1,24 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
+const msg = {"message": "Hello json"};
+//console.log("Hello World");
 
-console.log("Hello World");
+// app.get('/', (req, res) => {
+//     res.send('Hello Express');
+// })
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/json', (req, res) => {
+    if(process.env.MESSAGE_STYLE === 'uppercase') {
+        msg.message = msg.message.toUpperCase();
+    }; 
+        res.json(msg);
+});
+
+app.use('/public', express.static(__dirname + '/public'));
 
 module.exports = app;
