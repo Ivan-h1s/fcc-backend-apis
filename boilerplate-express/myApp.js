bb4b69fd6bf95e581a1f7a91fe42003bbdbb646f
Ivan Altamirano <pedrotrill@gmail.com>
2023-02-08 06:20:49 -0300

    express

---
+++ boilerplate-express/myApp.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let app = express();
+let server = require('./server');
+
+console.log("Hello World");
+
+// app.get('/', (req, res) => {
+//     console.log("Hello World");
+// });
+
+module.exports = app;
